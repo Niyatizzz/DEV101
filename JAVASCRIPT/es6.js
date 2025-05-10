0ee9f1a9256004e79aa4e5520affc990bc06f3bd
Niyatizzz <niyatikolukula@gmail.com>
2025-05-10 16:21:21 +0530

    git

--- JAVASCRIPT/es6.js
+++ JAVASCRIPT/es6.js
@@ -10,3 +10,39 @@ const makeError = () => {
     }
 }
 makeError()
+
+// web storage api
+
+//not part of dom, refers to the window api
+
+// window.alert() same as alret()
+
+// modules - introduced in es6
+// export import files
+
+export default function playGuitar() {
+    return 'playing guitar!';
+}
+
+export const shredding = () => {
+    return 'shredding';
+}
+
+export const plucking = () => {
+    return 'plucking strings';
+}
+
+// export{shredding, plucking};
+
+//importing
+
+//import playguitat from "./es6.js;" - like this
+
+//fetch api requires callbacks, promises, async/wait
+
+//callbacks
+
+function firstfunc(parameters, callback) {
+    //do task
+    callback();
+}
